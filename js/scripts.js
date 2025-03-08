0b5180a09ab85dd275cb5f4ae5f1d7a438cde149
juan <juandavidcaicedo1707@gmail.com>
2025-03-07 22:29:26 -0500

    se agrego en el condicional otras 3 opciones y en el index se agregaron las secciones correspondientes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 function mostrarDetalle(producto) {
+    console.log("JS cargado correctamente");
+
     let imagen = document.getElementById("detalleImagen");
     let descripcion = document.getElementById("detalleDescripcion");
+    let detalle = document.getElementById("detalle");
 
     // Asignar imagen y descripción según el producto
-    
     if (producto === "electro1") {
-        imagen.src = "assets/img/electro1.jpg";
+        imagen.src = "Lavadora.jpg";
         descripcion.textContent = "Refrigeradora moderna con sistema inverter.";
     } else if (producto === "electro2") {
-        imagen.src = "assets/img/electro2.jpg";
-        descripcion.textContent = "Lavadora de alta eficiencia con carga frontal.";
+        imagen.src = "licuadora.jpg";
+        descripcion.textContent = "Licuadora potente con múltiples velocidades.";
     } else if (producto === "electro3") {
-        imagen.src = "assets/img/electro3.jpg";
+        imagen.src = "microondas.jpg";
         descripcion.textContent = "Microondas digital con funciones avanzadas.";
+    } else if (producto === "electro4") {
+        imagen.src = "Nevera.jpg";
+        descripcion.textContent = "Nevera con gran capacidad y sistema de enfriamiento eficiente.";
+    } else if (producto === "electro5") {
+        imagen.src = "Plancha.jpg";
+        descripcion.textContent = "Plancha a vapor con tecnología antiadherente.";
+    } else if (producto === "electro6") {
+        imagen.src = "Ventilador.jpg";
+        descripcion.textContent = "Ventilador de torre con control remoto.";
     }
 
-    // Mostrar el cuadro de detalle
-    document.getElementById("detalle").classList.remove("oculto");
+    // Mostrar el cuadro emergente
+    detalle.classList.remove("oculto");
 }
 
 function cerrarDetalle() {
